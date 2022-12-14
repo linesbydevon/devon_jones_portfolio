241d00f66d0e9f6b068561b91ff79d07030b9c00
Devon Jones <linesbydevon@gmail.com>
2022-12-14 15:24:59 -0500

    added photo and added hamburger

--- drj_portfolio/src/App.js
+++ drj_portfolio/src/App.js
@@ -1,15 +1,18 @@
 import Header from "./components/Header";
 import Main from "./components/Main";
+import Nav from "./components/Nav"
 import "./App.css";
 
 function App() {
   return (
+      
       <div className="gradientBorder">
         <div className="wrapper">
           <Header/>
           <Main/>
         </div>
       </div>
+      
     
   );
 }
