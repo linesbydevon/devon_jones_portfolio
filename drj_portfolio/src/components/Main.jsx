7177bc3492929ff71c6e6dfb81b1639d141244c0
Devon Jones <linesbydevon@gmail.com>
2022-12-14 21:15:45 -0500

    started laying out projects section

--- drj_portfolio/src/components/Main.jsx
+++ drj_portfolio/src/components/Main.jsx
@@ -1,12 +1,13 @@
 import {Routes,Route} from 'react-router-dom'
 import Resume from './Routes/Resume'
+import Projects from './Routes/Projects'
 
 export default function Main(){
   return(
     <main>
       <Routes>
         <Route path="/"/>
-        <Route path="/projects" />
+        <Route path="/projects" element={<Projects/>} />
         <Route path="/resume" element={<Resume/>} />
       </Routes>
     </main>
---
+++ drj_portfolio/src/components/Routes/Projects.jsx
@@ -0,0 +1,58 @@
+export default function Projects() {
+  return (
+    <section className="projects">
+      <h2>Projects</h2>
+      <div className="gradientBorder projectContainer">
+        <div className="project">
+          <div className="projectInfo">
+            <h3>Tic-Tac-Toe</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <div className="buttons"></div>
+          </div>
+          <div className="thumbnailContainer">
+            <img />
+          </div>
+        </div>
+      </div>
+      <div className="gradientBorder  projectContainer">
+        <div className="project">
+          <div className="projectInfo">
+            <h3>Tic-Tac-Toe</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <div className="buttons"></div>
+          </div>
+          <div className="thumbnailContainer">
+            <img />
+          </div>
+        </div>
+      </div>
+      <div className="gradientBorder  projectContainer">
+        <div className="project">
+          <div className="projectInfo">
+            <h3>Tic-Tac-Toe</h3>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat.
+            </p>
+            <div className="buttons"></div>
+          </div>
+          <div className="thumbnailContainer">
+            <img />
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
